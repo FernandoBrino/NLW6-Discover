2b932bac9618933c7d01446cdb6dc1b0bba97b3b
Fernando Brino <fernandobrino11@gmail.com>
2021-07-23 11:50:13 -0300

    Database initialized and Insert data in Database

--- src/db/init.js
+++ src/db/init.js
@@ -7,16 +7,16 @@ const initDb = {
         // após a finalização e a respost da função que o contém (neste caso o 'Database()')
         // o 'async' precisa estar na função que o 'await' está dentro (no caso 'init()')
 
-        await db.exec(`CREATE TABLE rooms () {
+        await db.exec(`CREATE TABLE rooms (
             id INTEGER PRIMARY KEY,
             pass TEXT
-        }`);
+        )`);
 
-        await db.exec(`CREATE TABLE questions () {
+        await db.exec(`CREATE TABLE questions ( 
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             titulo TEXT,
-            read INTEGER
-        }`);
+            read INT
+        )`);
 
         await db.close()
 

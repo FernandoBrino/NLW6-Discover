f164f5f3294267d2ec012411d9087d70078860ae
Fernando Brino <fernandobrino11@gmail.com>
2021-07-25 16:19:55 -0300

    Finished Enter-room and No Questions CSS

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -48,8 +48,21 @@ module.exports = {
         const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 0`)
         // Seleciona todas as question que tem o room = roomId que desejamos
         const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 1`)
+        let isNoQuestions
 
-        res.render("room", {roomId: roomId, questions: questions, questionsRead: questionsRead})
+        if(questions == 0){
+            if(questionsRead == 0){
+                isNoQuestions = true
+            }
+        }
 
+        res.render("room", {roomId: roomId, questions: questions, questionsRead: questionsRead, isNoQuestions: isNoQuestions})
+
+    },
+
+    enter(req, res) {
+        const roomId = req.body.roomId
+        
+        res.redirect(`/room/${roomId}`)
     }
 }
